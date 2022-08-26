7ee3065f165b083da09b75e6236326aa15250362
kDedKd <kDedKd@cyber-dojo.org>
2022-08-26 08:25:06 +0000

    6 ran tests, no prediction, got amber

--- files/hiker.test.js
+++ files/hiker.test.js
@@ -7,3 +7,11 @@ describe('answer', () => {
     expect(answer()).toEqual(42);
   });
 });
+
+describe('fizzbuzz', () => {
+  it('to get fizz and buzz', () => {
+    const fizzBuzzArrayDump[] = [{"1", "2", "Fizz"}];
+    const fizzBuzzArray[] = getFizzBuzzList();
+    expect(answer()).toEqual(42);
+  });
+});
