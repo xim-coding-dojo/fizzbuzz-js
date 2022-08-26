dd6896501bd36af82f7e81a4e4d9f1cef9b908ca
kDedKd <kDedKd@cyber-dojo.org>
2022-08-26 09:00:44 +0000

    29 ran tests, no prediction, got green

--- files/hiker.js
+++ files/hiker.js
@@ -9,7 +9,7 @@ function getFizzBuzzList() {
   const resultArray = Array.from({length: 100}, (x, i) => i+1)
   .map(i=>(i % 3) === 0 ? "Fizz" : i)
   .map(i=>(i % 5) === 0 ? "Buzz" : i)
-  .map(i=>i);
+  .map(i=>i+"");
   
   
   
