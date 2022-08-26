04e4a1646f1304d73b9a1c40f327ee18d7a9f32a
kDedKd <kDedKd@cyber-dojo.org>
2022-08-26 09:40:57 +0000

    34 ran tests, no prediction, got amber

--- files/hiker.js
+++ files/hiker.js
@@ -5,16 +5,23 @@ function answer() {
 }
 
 function getFizzBuzzList() {
+  const mapToFizzForModulo3 = mapModuloToString(3,"Fizz")
+  
   
   const resultArray = Array.from({length: 100}, (x, i) => i+1)
-  .map(i=>(i % 3) === 0 ? "Fizz" : i)
+  .map(mapToFizzForModulo3(i))
   .map(i=>(i % 5) === 0 ? "Buzz" : i)
   .map(i=>(i % 3*5) === 0 ? "FizzBuzz" : i)
   .map(i=>i+"");
   
+  function mapModuloToString(modulo, string)
+  {
+    return (number) {
+      return (number % modulo) === 0 ? string
+    }
+  }
   
-  
-  var resultArray2 = [];
+  /* var resultArray2 = [];
   
   for (var i = 1; i <= 100; i++)
   {
@@ -32,7 +39,7 @@ function getFizzBuzzList() {
       resultArray2.push(i + "");
     }
     
-    
+  */
     
   }
   
