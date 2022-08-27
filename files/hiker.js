9a4b73421ef4041338f3f21239c2ff0870f86681
Rainer Blessing <rainer.blessing@posteo.de>
2022-08-27 11:26:10 +0000

    Loesung von Andreas und Marco

--- files/hiker.js
+++ files/hiker.js
@@ -5,44 +5,20 @@ function answer() {
 }
 
 function getFizzBuzzList() {
-  
   const mapToFizzForModulo3 = mapModuloToString(3,"Fizz");
   const mapToBuzzForModulo5 = mapModuloToString(5,"Buzz");
-  const mapToFizzBuzzForModulo15 = mapModuloToString(15,"Buzz");
-  
-  
+  const mapToFizzBuzzForModulo15 = mapModuloToString(15,"FizzBuzz");
   const resultArray = Array.from({length: 100}, (x, i) => i+1)
   .map(mapToFizzBuzzForModulo15)
   .map(mapToBuzzForModulo5)
   .map(mapToFizzForModulo3)
   .map(i=>i+"");
-  
   function mapModuloToString(modulo, string)
   {
     return (number) => (number % modulo) === 0 ? string : number;
   }
-  
-  /* var resultArray2 = [];
-  
-  for (var i = 1; i <= 100; i++)
-  {
-    
-    if ((i % 3) === 0)
-    {
-      resultArray2.push("Fizz");
-    }
-    else if ((i % 5) === 0)
-    {
-      resultArray2.push("Buzz");
-    }
-    else
-    {
-      resultArray2.push(i + "");
-    }
-    
-  } */
-  
   console.log(resultArray);
   return resultArray;
 }
+
 module.exports = getFizzBuzzList;
